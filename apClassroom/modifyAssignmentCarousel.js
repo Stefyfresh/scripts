22c4b02df652be08b04f9033489e885f5c6a7a38
StefyfreshSchool <sesposto@bayviewglen.ca>
2021-11-04 12:06:12 -0400

    Update info

--- apClassroom/modifyAssignmentCarousel.js
+++ apClassroom/modifyAssignmentCarousel.js
@@ -6,5 +6,9 @@ modifyCarousel = setInterval(() => {
     } 
 }, 500);
 
-//This script modifies the AP classroom assignment carousel by adding a scroll
-// bar for easy access to all the assignments.
+/*
+This script modifies the AP classroom assignment carousel by adding a scroll bar for easy access to all the assignments.
+
+To use it, install an HTTP request modifier like Requestly (https://requestly.io/) and point the script to run at page load.
+*/
+ 
