37b624817bbd47c50048560c7f1925a29ea1f231
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-04 21:27:06 -0400

    more info and console stuff

--- apClassroom/modifyAssignmentCarousel.js
+++ apClassroom/modifyAssignmentCarousel.js
@@ -1,7 +1,8 @@
+console.log("AP Classroom Modify Script: Page loaded, waiting for element.");
 modifyCarousel = setInterval(() => {
     if (document.getElementsByClassName("carousel-parent-container")[0] != undefined){
         document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
-    
+        console.log("AP Classroom Modify Script: Injection complete.");
         clearInterval(modifyCarousel);
     } 
 }, 500);
@@ -10,5 +11,7 @@ modifyCarousel = setInterval(() => {
 This script modifies the AP classroom assignment carousel by adding a scroll bar for easy access to all the assignments.
 
 To use it, install an HTTP request modifier like Requestly (https://requestly.io/) and point the script to run at page load.
+
+Make sure to click "Raw" when getting the link to the script or else it will not work.
 */
  
