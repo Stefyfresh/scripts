711db3b0d6b1fb43f7f6ddbccc7b0f50e587243b
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-04 23:53:32 -0400

    no work so bye bye

--- apClassroom/modifyAssignmentCarousel/testInjection.js
+++
@@ -1,5 +0,0 @@
-// This script is purely for testing purposes for modifyAssignmentCarousel.js. 
-
-// If you think the script isn't working, try this one to see if the injection is working.
-// This displays a pop-up when the script is run telling you if the injection worked.
-alert("Injection Success! Press OK to continue using the page.");
