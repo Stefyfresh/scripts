1ec6731b12aabc547c12bdb36c469b720742e780
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-04 22:00:03 -0400

    Update modifyAssignmentCarousel.js

--- apClassroom/modifyAssignmentCarousel.js
+++ apClassroom/modifyAssignmentCarousel.js
@@ -1,5 +1,6 @@
 console.log("AP Classroom Modify Script: Page loaded, waiting for element.");
 modifyCarousel = setInterval(() => {
+    // Assignment carousel element is the first element with class "carousel-parent-container".
     if (document.getElementsByClassName("carousel-parent-container")[0] != undefined){
         document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
         console.log("AP Classroom Modify Script: Injection complete.");
