17707d616bc156fd7a11320b28dc772217f308dc
Stefyfresh <90330171+StefyfreshSchool@users.noreply.github.com>
2021-11-04 23:46:02 -0400

    more robust it yes is

--- apClassroom/modifyAssignmentCarousel/modifyAssignmentCarousel.js
+++ apClassroom/modifyAssignmentCarousel/modifyAssignmentCarousel.js
@@ -1,9 +1,20 @@
-console.log("AP Classroom Modify Script: Page loaded, waiting for element.");
-modifyCarousel = setInterval(() => {
+console.log("AP Classroom Assignments Modify: Page loaded and script loaded, waiting for element.");
+let check = true;
+modifyCarousel = setInterval(()=>{
     // Assignment carousel element is the first element with class "carousel-parent-container".
     if (document.getElementsByClassName("carousel-parent-container")[0] != undefined){
-        document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
-        console.log("AP Classroom Modify Script: Injection complete.");
-        clearInterval(modifyCarousel);
-    } 
+        if (check == true){
+            // If we are checking and the element exists, enable the scroll bar and stop checking.
+            document.getElementsByClassName("carousel-parent-container")[0].style.overflowX = "scroll";
+            console.log("AP Classroom Assignments Modify: Carousel found! Adding scroll bar.");
+            check = false;
+        }
+    } else {
+        // Only log to the console the first time the element disappears.
+        if (check == false){
+            console.log("AP Classroom Assignments Modify: Carousel disappeared! Waiting for element.");
+        }
+        // If the element does not exist we should check for it.
+        check = true;
+    }
 }, 500);
